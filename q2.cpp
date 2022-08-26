ddad7380c504c0fb3c530f974a5f389aed3e6ebf
Kashish2502 <kashish0375.be20@chitkara.edu.in>
2022-08-26 14:01:14 +0530

    cpptest

--- q2.cpp
+++ q2.cpp
@@ -5,77 +5,40 @@
   */
 
  
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// bool checkCons(int arr[],int n){
-//   sort(arr,arr+n);
-//   for(int i=1;i<n;i++){
-//     if(arr[i]!=arr[i-1]+1){
-//       return false;
-//       }
-//   }
-//   return arr[i];
-// }
-
-// int main()
-// {
-//     int n;
-//     cin>>n;
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
-//   if(checkCons(arr,n)==true){
-//     cout<<"true";
-//   }
-//   else
-//     cout<<"false";
-
-//     return 0;
-// }
-
-#include <bits/stdc++.h>
+ #include<iostream>
+#include<unordered_set>
 using namespace std;
-
-bool Consecutive(int arr[], int n)
-{
-	
-	sort(arr,arr+n);
-    
-	
-	for(int i=1;i<n;i++)
-	{
-		if(arr[i]!=arr[i-1]+1)
-		{
-			return false;
-		}
-	}
-	return true;
+void consecutive(int arr[],int n){
+    if(n<=1){
+        cout<<"The array contain consecutive number"<<endl;
+    }
+    int min=arr[0];
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+    if(arr[i]<min){
+        min=arr[i];
+    }
+    if(arr[i]>max){
+        max=arr[i];
+    }
+    }
+    if(max - min != n -1){
+       unordered_set<int> visited;
+    for (int i = 0; i < n; i++){
+        if (visited.find(arr[i]) != visited.end()) {
+            cout<<"The array does not contains consecutive integers as element "<<arr[i]<<" is repeated"<<endl;
 }
-
-bool rep(int arr[],int n){
-    sort(arr,arr+n);
-     for(int i = 0; i < n; i++)
-    for(int j = i+1; j < n; j++)
-      if(arr[i] == arr[j])
-        return arr[i];
+        visited.insert(arr[i]);
+    }
+    }
+    else{
+         cout<<"The array contain consecutive integers from "<<min<<" to "<<max<<endl;
+    }
+    
+    
 }
-
-
-int main()
-{
-	// int arr[]= {-1,5,4,2,0,3,1};
-    int arr[]={4,2,4,3,1};
-	int n = sizeof(arr)/sizeof(arr[0]);
-    
-    
-    
-	if(Consecutive(arr, n) == true){
-		cout<<" The Array contains  consecutive integers from "<<arr[0]<<" to "<<arr[n-1];}
-	else if(rep(arr,n)== true){
-        int repeated = rep(arr,n);
-		cout<<" Array array does  not contain  consecutive integer as element " <<arr[n-1]<<"  is repeated ";}
-	return 0;
+int main(){
+    int arr[]={-1,5,4,2,0,3,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    consecutive(arr,n);
 }
-
